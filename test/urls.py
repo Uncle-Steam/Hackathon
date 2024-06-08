fce19282233e31280b184d9750fa894d73f04c21
Weipin Ang <80883634+WeipinAng@users.noreply.github.com>
2024-06-08 13:24:21 +0800

    create login page index.html

--- test/urls.py
+++ test/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name = 'site-home'),
-    path('about/', views.about, name = 'site-about')
+    path('', views.index, name='site-index'),
+    path('about/', views.about, name='site-about')
 ]
--- test/views.py
+++ test/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-#request -> response (handler)
+# request -> response (handler)
 
 user = {
-        'username': 'Tritop21',
-        'type': 'Startup'
-    }
+    'username': 'Tritop21',
+    'type': 'Startup'
+}
 
 abouts = {
     'cocreators': ['Ananda', 'Averil', 'Janson', 'Weipin', 'Winsen']
 }
+
 
 def home(request):
     context = {
@@ -18,8 +19,16 @@ def home(request):
     }
     return render(request, "test.html", context)
 
+
 def about(request):
     context = {
         'data': abouts
     }
     return render(request, "about.html", context)
+
+
+def index(request):
+    context = {
+        'data': index
+    }
+    return render(request, "index.html", context)
