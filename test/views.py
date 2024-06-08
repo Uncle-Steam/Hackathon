f5824bdbc520c2e3960eebef196b8b22a064eec5
Tritop21 <anandamulya460@gmail.com>
2024-06-08 22:28:59 +0800

    added dummy and prompt logic

--- test/views.py
+++ test/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
-# request -> response (handler)
+import ai
+import trialData
 
 user = {
     'username': 'Tritop21',
@@ -33,4 +33,25 @@ def login(request):
         return render(request, 'signup.html')
     else:
         return render(request, 'index.html')
+    
+
+def firstGenerate(request):
+    about = request.GET['about']
+    objective = request.GET['objective']
+    industry = request.GET['industry']
+    name = request.GET['name']
+    regisNumber = request.GET['regisNumber']
+    foundedDate = request.GET['foundedDate']
+    social = request.GET['social']
+    
+    concactenate = ""
+    num = 1
+    
+    for i in trialData.users:
+        concactenate += f"User {num} is planning to invest in {i['planning']}, is interested in {i['industry']} sectors, and follows {i['followed']} on LinkedIn.\n"
+        num += 1
+    
+    prompt = f"You are analyzing a startup company and how likely they are to attract certain investor profiles based on their 
+    About Us, objective, industry, name and their social cause. Here is their profile About Us:{about}, Objective:{objective}, Industry:{industry}, 
+    Name:{name}, Social Cause:{social}. Make a compatibility analysis with the user profile provided which is: {concactenate}"
     
