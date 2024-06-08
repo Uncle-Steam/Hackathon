5363855f9eae06f0d3b1e63ca6418cdd7a6f6eaf
Tritop21 <anandamulya460@gmail.com>
2024-06-08 12:39:44 +0800

    dunno

--- test/views.py
+++ test/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from django.http import HttpResponse
-# Create your views here.
-#request -> response (handler)
 
 user = {
         'username': 'Tritop21',
