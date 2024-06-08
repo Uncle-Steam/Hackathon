2ff75b51b0d45f6a8c375d16202e1ddeaa67e8f8
Tritop21 <anandamulya460@gmail.com>
2024-06-08 16:18:31 +0800

    ananda experiments

--- test/urls.py
+++ test/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name = 'site-home'),
-    path('about/', views.about, name = 'site-about')
+    path('about/<str:pk>/', views.about, name = 'site-about'),
+    path('add', views.add, name='add')
 ]
--- test/views.py
+++ test/views.py
@@ -11,14 +11,38 @@ abouts = {
     'cocreators': ['Ananda', 'Averil', 'Janson', 'Weipin', 'Winsen']
 }
 
+types = [
+    {'id': 1, 'name': 'Room One'},
+    {'id': 2, 'name': 'Room Two'},
+    {'id': 3, 'name': 'Room Three'},
+]
+
 def home(request):
     context = {
-        'data': user
+        'data': user,
+        'types': types
     }
     return render(request, "test.html", context)
 
-def about(request):
+def about(request, pk):
+    type = None
+    for i in types:
+        if i['id'] == int(pk):
+            type = i
     context = {
+        'type': type,
         'data': abouts
     }
     return render(request, "about.html", context)
+
+def add(request):
+    
+    val1 = int(request.GET['num1'])
+    val2 = int(request.GET['num2'])
+    res = val1+val2
+    
+    context = {
+        'res': res
+    }
+    
+    return render(request, 'about.html', context)
